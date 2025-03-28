1311cd4fdd7ee95a8b2b8dc709c00338969574d9
converter13 <imlikissa@gmail.com>
2025-03-29 01:40:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function convertToPDF() {
+async function convertToPDF() {
+    const { jsPDF } = window.jspdf;
     let input = document.getElementById('fileInput').files;
+
     if (input.length === 0) {
         alert("Please select a file!");
         return;
@@ -8,17 +10,20 @@ function convertToPDF() {
     let doc = new jsPDF();
     for (let i = 0; i < input.length; i++) {
         let file = input[i];
-        let reader = new FileReader();
-        
-        reader.onload = function(e) {
-            let img = new Image();
-            img.src = e.target.result;
-            img.onload = function() {
-                doc.addImage(img, 'JPEG', 10, 10, 180, 160);
-                if (i < input.length - 1) doc.addPage();
-                else doc.save('converted.pdf');
+        if (file.type.startsWith("image/")) {
+            let reader = new FileReader();
+            reader.onload = function(e) {
+                let img = new Image();
+                img.src = e.target.result;
+                img.onload = function() {
+                    doc.addImage(img, 'JPEG', 10, 10, 180, 160);
+                    if (i < input.length - 1) doc.addPage();
+                    else doc.save('converted.pdf');
+                };
             };
-        };
-        reader.readAsDataURL(file);
+            reader.readAsDataURL(file);
+        } else {
+            alert("Only image files are supported!");
+        }
     }
 }
